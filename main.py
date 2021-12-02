48c8a71cc421e169911dd05aa0ce6b85671f2839
Andre Nalbandian <andrenalbandian@gmail.com>
2021-12-01 21:42:59 -0800

    Real time mic recog done

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("hey")
+import speech_recognition as sr
+import keyboard
+
+r = sr.Recognizer()
+mic = False
+if(mic):
+    filename = "machine-learning_speech-recognition_16-122828-0002.wav"
+    # initialize the recognizer
+    with sr.AudioFile(filename) as source:
+        audio_data = r.record(source)
+        text = r.recognize_google(audio_data)
+        print(text)
+else:
+    print("speak anything: ")
+    try:
+        while(True):
+            with sr.Microphone() as source:
+                audio = r.listen(source)
+                text = r.recognize_google(audio)
+                print(text)
+    except KeyboardInterrupt:
+        pass
