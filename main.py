5febebbe2b52bf894f134dd613148826d124c172
Andre Nalbandian <andrenalbandian@gmail.com>
2021-12-08 13:24:54 -0800

    Created functions and added skeleton

--- main.py
+++ main.py
@@ -13,40 +13,49 @@ import keyboard
 import os
 
 keywords = ["My name is", "student", "years", "experience", "", "", "", "", "",]
+r = sr.Recognizer()
 
-class TextualAnalysis():
-    r = sr.Recognizer()
+def TextProcess(text):
+    #outputs a confidence rating to the interview question
     count = 0
-    mp4File = "BroadcastTest.mp4"
-    #mp4File = GET REQUEST HERE TO GET THE MP4 FILE
+    print(text)
+    words = text.split()
+    for word in words:
+        if word.lower() in keywords:
+            count += 1
+    print(count/(len(words)))
 
-    #GET THE USERID TO CREATE NEW FILES
-    userID = 1
+def writeToFile(text):
+    #writes text to the txt file
+    file = open("SpeechToText" + str(userID) + ".txt", "a+")
+    file.write(text)
+    file.write("\n")
+    file.close()
 
-    clip = mp.VideoFileClip(mp4File)
-    clip.audio.write_audiofile("BroadcastTestConv.wav", codec='pcm_s16le')
-
-    filename = "BroadcastTestConv.wav"
-
-    # initialize the recognizer
-    with sr.AudioFile(filename) as source:
-        audio_data = r.record(source)
-        text = r.recognize_google(audio_data)
-        print(text)
-        #POST REQUEST TO SEND THE STRING TO THE BOT
-
-        words = text.split()
-        for word in words:
-            if word.lower() in keywords:
-                count += 1
-        print(count/(len(words)))
-
-        file = open("SpeechToText" + str(userID) + ".txt", "a+")
-        file.write(text)
-        file.write("\n")
-        file.close()
-
+def removeFiles():
+    #removes .wav and .mp4 files from computer
     os.remove(filename)
     #os.remove(mp4File)
 
-    #SEND OUT TEXT FILE IF INTERVIEW IS COMPLETE USING PUT REQUEST
+#mp4File = GET REQUEST HERE TO GET THE MP4 FILE
+mp4File = "BroadcastTest.mp4"
+
+#GET THE USERID TO CREATE NEW FILES
+userID = 1
+
+clip = mp.VideoFileClip(mp4File)
+clip.audio.write_audiofile("Convert.wav", codec='pcm_s16le')
+
+filename = "Convert.wav"
+
+# initialize the recognizer
+with sr.AudioFile(filename) as source:
+    audio_data = r.record(source)
+    text = r.recognize_google(audio_data)
+    #POST REQUEST TO SEND THE STRING TO THE BOT
+    TextProcess(text)
+    writeToFile(text)
+
+removeFiles()
+#SEND OUT TEXT FILE IF INTERVIEW IS COMPLETE USING PUT REQUEST 
+
